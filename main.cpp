c80ed3f19bdb97be6e0755bfa1f146897a502093
stastez <78495486+Stastez@users.noreply.github.com>
2024-03-29 01:05:38 +0100

    Choose CPU or GPU via cli

--- main.cpp
+++ main.cpp
@@ -18,10 +18,13 @@ int main(const int argc, const char *argv[]) {
 	} else if (argc < 5) {
 		std::cerr << "Provide both a width and height resolution!\n";
 		return 1;
+	} else if (argc < 6) {
+		std::cerr << "Provide whether to use the GPU!\n";
+		return 1;
 	}
 
 	int debug;
-	if (argc > 5) debug = std::max(std::stoi(argv[5]), EFTDEM_DEBUG);
+	if (argc > 6) debug = std::max(std::stoi(argv[6]), EFTDEM_DEBUG);
 	else debug = EFTDEM_DEBUG * 25;
 
 	EFTDEM::PointCloud pointCloud{std::stoul(argv[3]), std::stoul(argv[4])};
@@ -34,8 +37,12 @@ int main(const int argc, const char *argv[]) {
 
 	// Trigger write back to host on buffer destruction
 	{
+		sycl::queue queue;
+		if (std::stoi(argv[5])) queue = sycl::queue{sycl::gpu_selector_v, sycl::property::queue::in_order{}};
+		else queue = sycl::queue{sycl::cpu_selector_v, sycl::property::queue::in_order{}};
+
 		EFTDEM::SYCLState syclState{
-			sycl::queue{sycl::gpu_selector_v, sycl::property::queue::in_order{}},
+			queue,
 			{pointCloud.points},
 			{sycl::range<1>{pointCloud.points.size()}},
 			// This does not initialize heightsBuffer with the values in heights
@@ -48,7 +55,7 @@ int main(const int argc, const char *argv[]) {
 
 		EFTDEM::Rasterizer::rasterizePointCloud(pointCloud, syclState, debug);
 
-		EFTDEM::RadialFiller::fill(pointCloud, syclState, 50, debug);
+		EFTDEM::RadialFiller::fill(pointCloud, syclState, 300, debug);
 	}
 
 	const auto endTime = std::chrono::high_resolution_clock::now();
